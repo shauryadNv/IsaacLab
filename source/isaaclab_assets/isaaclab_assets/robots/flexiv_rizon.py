0dd231eb86c87071e1bbfb7fa43e4438b4cec397
shauryadNv <shauryad@nvidia.com>
2026-02-24 12:45:40 -0800

    Fixed import error.

--- source/isaaclab_assets/isaaclab_assets/robots/flexiv_rizon.py
+++ source/isaaclab_assets/isaaclab_assets/robots/flexiv_rizon.py
@@ -19,6 +19,7 @@ import math
 import isaaclab.sim as sim_utils
 from isaaclab.actuators import ImplicitActuatorCfg
 from isaaclab.assets.articulation import ArticulationCfg
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR, ISAACLAB_NUCLEUS_DIR
 
 ##
 # Configuration
